cff50feb209a96093773bf576ebf1b63745ddb12
yogeshwaran-98 <mryogeshwaran98@gmail.com>
2026-06-12 13:37:16 +0530

    terraform vpc/main updated

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -13,7 +13,7 @@ resource "aws_vpc" "this" {
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -26,7 +26,7 @@ resource "aws_subnet" "public_1" {
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "ap-south-1b"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
   tags = {
@@ -41,7 +41,7 @@ resource "aws_subnet" "public_2" {
 resource "aws_subnet" "private_1" {
   vpc_id            = aws_vpc.this.id
   cidr_block        = "10.0.11.0/24"
-  availability_zone = "ap-south-1a"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name                                        = "private-1"
@@ -53,7 +53,7 @@ resource "aws_subnet" "private_1" {
 resource "aws_subnet" "private_2" {
   vpc_id            = aws_vpc.this.id
   cidr_block        = "10.0.12.0/24"
-  availability_zone = "ap-south-1b"
+  availability_zone = "us-east-1b"
 
   tags = {
     Name                                        = "private-2"
